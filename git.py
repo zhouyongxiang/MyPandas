10758e2c699308b9bc1a9fb38ca66285c443081b
zhou <13435320012@139.com>
2019-06-13 07:00:10 +0800

    完好

--- git.py
+++ git.py
@@ -1,21 +1,22 @@
 import os
 
-class My_git:
-	def	__init__(self):
-		self.start
-	def git_push(self):
-		os.system("git add -A")
-		str = input("输入上传信息：")
-		os.system("git commit -m"+str)
-		os.system("git push")
-	def git_pull(self):
-		os.system("git pull")
-	def start(self):
-		list = [{"上传":self.git_push},{"下载":self.git_pull},{"退出":exit}]
-		while 1:
-			for i,j in enumerate(list,1):
-				print(i,j.keys)
-			chose= input("请选择>>>")
-			getattr(list(int(chose)-1))
-
-g = My_git()
+def git_push():
+    os.system("git add -A")
+    str = input("输入上传信息：")
+    os.system("git commit -m"+str)
+    os.system("git push")
+def git_pull():
+    os.system("git pull")
+def git_exit():
+    exit()
+while 1:
+    list = [("上传","git_push"),("下载","git_pull"),("退出","git_exit")]
+    for i,j in enumerate(list,1):
+        print(i,j[0])
+    choise = input("请选择>>>").strip()
+    if choise.isdigit():
+        choise = int(choise) - 1
+        if choise <= len(list)+1:
+            locals()[list[choise][1]]()
+    else:
+        print("类型错误")
