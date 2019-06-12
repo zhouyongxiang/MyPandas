a060793db1c4b3fedf4f526ddb7288d279b0e0c0
zhou <529692095@qq.com>
2019-06-12 11:53:05 +0800

    11

--- git.py
+++ git.py
@@ -1,5 +1,21 @@
 import os
-os.system("git add -A")
-str = input("输入上传信息")
-os.system("git commit -m"+str)
-os.system("git push")
+
+class My_git:
+	def	__init__(self):
+		self.start
+	def git_push(self):
+		os.system("git add -A")
+		str = input("输入上传信息：")
+		os.system("git commit -m"+str)
+		os.system("git push")
+	def git_pull(self):
+		os.system("git pull")
+	def start(self):
+		list = [{"上传":self.git_push},{"下载":self.git_pull},{"退出":exit}]
+		while 1:
+			for i,j in enumerate(list,1):
+				print(i,j.keys)
+			chose= input("请选择>>>")
+			getattr(list(int(chose)-1))
+
+g = My_git()
