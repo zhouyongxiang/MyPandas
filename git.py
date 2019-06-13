b4cde7d83ba5be928b790a6b97af64bad715057a
zhou <529692095@qq.com>
2019-06-13 11:38:48 +0800

    s

--- git.py
+++ git.py
@@ -7,17 +7,20 @@ def git_push():
     os.system("git push")
 def git_pull():
     os.system("git pull")
-def git_exit():
-    exit()
+def git_status():
+    os.system("git status")
+
 while 1:
-    list = [("上传","git_push"),("下载","git_pull"),("退出","git_exit")]
+    list = [("上传",git_push),("下载",git_pull),("状态",git_status),("退出",exit)]
     print("------------  git  -------------")
     for i,j in enumerate(list,1):
         print(i,j[0])
     choise = input("请选择>>>").strip()
     if choise.isdigit():
         choise = int(choise) - 1
-        if choise <= len(list)+1:
-            locals()[list[choise][1]]()
+        if choise < len(list) and choise>0:
+            list[choise][1]()
+        else:
+            print("没有此菜单")
     else:
         print("类型错误")
