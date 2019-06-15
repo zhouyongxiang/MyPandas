bf8f4572bea705e01cfc4e9c0c6de47e7bb039a5
zhou <13435320012@139.com>
2019-06-15 23:15:23 +0800

    git_bash

--- git.py
+++ git.py
@@ -7,11 +7,19 @@ def git_push():
     os.system("git push")
 def git_pull():
     os.system("git pull")
+def git_bash():
+	while 1:
+		print("输入2退出")
+		str = input("git指令>>>")
+		if str != "2":
+			os.system(str)
+		else:
+			break
 def git_status():
     os.system("git status")
 
 while 1:
-    list = [("上传",git_push),("下载",git_pull),("状态",git_status),("退出",exit)]
+    list = [("上传",git_push),("下载",git_pull),("状态",git_status),("bash",git_bash),("退出",exit)]
     print("------------  git  -------------")
     for i,j in enumerate(list,1):
         print(i,j[0])
